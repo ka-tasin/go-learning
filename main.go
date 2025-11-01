3b1d58015c5d25aa0f5faf9ea912c42b3d88e77b
ka-tasin <kausarahmadtasinkat@gmail.com>
2025-11-01 21:31:45 +0600

    function and scope

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+var num1 int = 13
+var num2 int = 45
+
+func add(num1 int, num2 int) int {
+	return num1 + num2
+}
+
 func main() {
+	var a int = 2
+	var b int = 4
 	fmt.Println("Hello World")
+
+	fmt.Println(add(num1, num2))
+
+	fmt.Println(add(34, 745))
+
+	fmt.Println(add(a, b))
 }
